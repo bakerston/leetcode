7fbab09275f7841b27d9393f5692890a33d1de63
bakerston <38169559+bakerston@users.noreply.github.com>
2019-11-23 09:28:38 -0500

    Add files via upload

---
+++ 024.Swap Nodes in Pairs.py
@@ -0,0 +1,7 @@
+pre, pre.next = self, head
+    while pre.next and pre.next.next:
+        a = pre.next
+        b = a.next
+        pre.next, b.next, a.next = b, a, b.next
+        pre = a
+    return self.next
---
+++ 50. Pow(x, n).py
@@ -0,0 +1,1 @@
+def myPow(x, n):
