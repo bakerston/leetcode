c5d444090e90065155d2ddb7ccf9862d205c014d
bakerston <38169559+bakerston@users.noreply.github.com>
2019-12-20 00:06:11 -0500

    Add files via upload

---
+++ 303rangeSumQuery.py
@@ -0,0 +1,12 @@
+self.accu = [0]
+        for num in nums:
+            self.accu += self.accu[-1] + num,
+
+    def sumRange(self, i, j):
+        """
+        sum of elements nums[i..j], inclusive.
+        :type i: int
+        :type j: int
+        :rtype: int
+        """
+        return self.accu[j + 1] - self.accu[i]
