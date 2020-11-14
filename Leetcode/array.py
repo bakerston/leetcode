027fdc23391b04132299ae0c31e6af52135183f5
bakerston <38169559+bakerston@users.noreply.github.com>
2020-11-14 15:58:47 -0500

    Add files via upload

--- Leetcode/array.py
+++ Leetcode/array.py
@@ -158,6 +158,7 @@ print(oddCells(2,2,[[1,1],[0,0]]))
 """
 
 #1436. Destination City
+"""
 def destCity(paths):
     s=[]
     d=[]
@@ -170,6 +171,169 @@ def destCity(paths):
         if i not in sset:
             return i
     
-print(destCity([["B","C"],["D","B"],["C","A"]]))
+print(destCity([["B","C"],["D","B"],["C","A"]]))"""
 
-        
+#1395. Count Number of Teams
+"""
+def numTeams(rating):
+    ans=0
+    def lowhighthan(num,alist):
+        return sum(map(lambda x:x<num, alist)), sum(map(lambda x:x>num, alist))
+    for num in range(1,len(rating)-1):
+        l=rating[:num]
+        r=rating[num+1:]
+        lres=lowhighthan(rating[num],l)
+        rres=lowhighthan(rating[num],r)
+        print(lres,rres)
+        ans+=lres[0]*rres[1]+lres[1]*rres[0]
+    return ans
+print(numTeams([2,5,3,4,1]))
+"""
+
+#1450. Number of Students Doing Homework at a Given Time
+"""
+def busyStudent(startTime, endTime, queryTime):
+    start=list(map(lambda x: x<=queryTime,startTime))
+    end=list(map(lambda x: x>=queryTime,endTime))
+    return sum(map(lambda x: x[0] and x[1], zip(start,end) ))
+print(busyStudent( [9,8,7,6,5,4,3,2,1],[10,10,10,10,10,10,10,10,10],5))
+"""
+
+#1646. Get Maximum in Generated Array
+"""
+def getMaximumGenerated(n):
+    if n==0:
+        return 0
+    elif n==1:
+        return 0
+    elif n==2:
+        return 1
+    else:
+        res=[0,1]
+        for i in range(2,n+1):
+            if i%2==0:
+                res.append(res[i//2])
+            else:
+                res.append(res[(i-1)//2]+res[(i-1)//2+1])
+    return max(res)
+print(getMaximumGenerated(2))"""
+
+#1535. Find the Winner of an Array Game
+"""def getWinner(arr, k):
+    alen=len(arr)
+    maxnum=max(arr)
+    
+
+    defender=arr[0]
+    if defender==maxnum:
+        return defender
+    else:
+        defending=True
+        tmp=0
+        challenger=1
+        while defending:
+            if defender>arr[challenger]:
+                tmp+=1
+                if tmp==k:
+                    return defender
+                challenger+=1
+            else:
+                defending=False
+    for i in range(1,alen):
+        if arr[i]>defender:
+            tmp=1
+            if tmp==k:
+                    return arr[i]
+            while 
+"""  
+    
+
+
+#1652. Defuse the Bomb
+"""
+def decrypt(code, k):
+    if len(code)==1:
+        return [0]
+    alen=len(code)
+    if k==0:
+        return [0 for x in code]
+    elif k>0:
+        ans=[]
+        base=sum(code[1:k+1])
+        ans.append(base)
+        for i in range(1,len(code)):
+            base-=code[(i)%alen]
+            base+=code[(i+k)%alen]
+            ans.append(base)
+    else:
+        ans=[]
+        base=sum(code[alen+k:])
+        print(base)
+        ans.append(base)
+        for i in range(1,len(code)):
+            base-=code[(alen+k-1+i)%alen]
+            print(base)
+            base+=code[i-1]
+            ans.append(base)
+    return ans
+print(decrypt([2,4,9,3],-2))
+"""
+
+#945. Minimum Increment to Make Array Unique
+"""def minIncrementForUnique(A):
+    alen=len(A)
+    if alen==1:
+        return 0
+    res=sorted(A)
+    mark=res[0]
+    ans=0
+    for i in range(1,alen):
+        if res[i]<=mark:  
+            mark+=1
+            ans+=mark-res[i]
+        mark=max(mark,res[i])
+    return ans
+print(minIncrementForUnique([3,1012,11,2,1,7]))"""
+
+#1346. Check If N and Its Double Exist  
+"""
+def checkIfExist(arr):
+    if 0 not in arr:
+        return len(set(arr+list(map(lambda x:x*2, arr))))!=2*len(set(arr))
+    else:
+        res=[x for x in arr if x!=0]
+        return len(set(res+list(map(lambda x:x*2, res))))!=2*len((res)) if len(arr)-len(res)==1 else True
+print(checkIfExist([-2,0,100,-19,4,6,-8]))
+"""
+
+#1217. Minimum Cost to Move Chips to The Same Position
+"""
+def minCostToMoveChips(position):
+    alen=len(position)
+    if alen==1:
+        return 0
+    else:
+        l=sum([1 for i in range(alen) if position[i]%2])
+        r=sum([1 for i in range(alen) if not position[i]%2])
+    return min(l,r)
+
+print(minCostToMoveChips([1,2,3,4,5,6]))"""
+
+#1508. Range Sum of Sorted Subarray Sums
+"""def rangeSum(nums, n, left, right):
+    res=[]
+    for i in range(n):
+        for j in range(i+1,n+1):
+            res.append(sum(nums[i:j]))
+    return sum(sorted(res)[left-1:right])
+print(16716700000%(10**9+7))"""
+
+#1465. Maximum Area of a Piece of Cake After Horizontal and Vertical Cuts
+def maxArea(h, w, horizontalCuts, verticalCuts):
+    hori=sorted([0]+horizontalCuts+[h])
+    vert=sorted([0]+verticalCuts+[w])
+    print(hori,vert)
+    h=max(map(lambda x:x[1]-x[0], zip(hori[:-1],hori[1:])))       
+    v=max(map(lambda x:x[1]-x[0], zip(vert[:-1],vert[1:]))) 
+    return (h*v)%(10**9+7)
+print(maxArea(5,4,[3,1],[1]))
