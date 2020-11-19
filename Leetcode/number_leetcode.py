8f023309650bc673c9b7df94d63b41e670b7c9de
bakerston <38169559+bakerston@users.noreply.github.com>
2020-11-19 17:09:49 -0500

    Add files via upload

---
+++ Leetcode/number_leetcode.py
@@ -0,0 +1,43 @@
+#1342. Number of Steps to Reduce a Number to Zero
+"""def numberOfSteps(num):
+    ans=0
+    while num!=0:
+        if num%2==1:
+            num-=1
+        else:
+            num/=2
+        ans+=1
+    return ans
+print(numberOfSteps(8))"""
+#1281. Subtract the Product and Sum of Digits of an Integer
+"""from functools import reduce
+
+def subtractProductAndSum(n):
+    res=list(map(lambda x:int(x),list(str(n))))
+    su=sum(res)
+    mu=reduce(lambda x,y:x*y,res)
+    return mu-su
+print(subtractProductAndSum(4421))"""
+"""
+def subtractProductAndSum(n):
+    alist=list(str(n))
+    mu=1
+    su=0
+    for i in range(len(alist)):
+        mu*=int(alist[i])
+        su+=int(alist[i])
+    return mu-su
+print(subtractProductAndSum(4421))"""
+
+#1518. Water Bottles
+"""
+def numWaterBottles(numBottles, numExchange):
+    ans=numBottles
+    ext=numBottles
+    while ext>=numExchange:
+        a,b=divmod(ext,numExchange)
+        ans+=a
+        ext=b+a
+    return ans
+print(numWaterBottles(9,3))
+"""
