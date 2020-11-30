72e71dbe3be14c1bf2551873ab2481ed9f7eca0d
bakerston <38169559+bakerston@users.noreply.github.com>
2020-11-29 19:16:37 -0500

    Add files via upload

---
+++ Leetcode/test2.py
@@ -0,0 +1,17 @@
+
+#c=sorted(alist,key=lambda x:[x[0],-x[1]])
+#alist=[["a",1],["a",2],["b",1],["b",3],["b",2],["c",1]]
+#c=[list(map(lambda x:x[1],g)) for k,g in itertools.groupby(zip(alist,blist),
+#cur=[list(g) for k, g in groupby(bloomDay, lambda x: x in t) if not k]
+#c=[8  if x==1 else 0 for x in nums]
+
+import bisect
+import math
+import functools
+import collections
+import re
+import itertools
+
+
+a="/a/d/c/cd"
+print(a.find("/a/b/c/cd/dad/"))
